f3bb9ab3b3eb6875dd264c43742e7fe99f099dbe
nssv-03 <nssv-03@MI-PC-047>
2014-05-28 11:03:38 +0900

    WeBAccess
    yahooニュースにアクセス

---
+++ KOWebCom/src/com/kobayasi/kowebcom/Webcom1.java
@@ -0,0 +1,48 @@
+package com.kobayasi.kowebcom;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Webcom1 extends Activity {
+	private TextView mView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_webcom1);
+		mView=(TextView)findViewById(R.id.view);
+		mView.setText(new String(httpGet("http://www.yahoo.co.jp")));
+		}
+		public String createURL(){
+			String apiURL="http://news.yahooapis.jp/NewsWebService/V2?topics?";
+			String appid ="dj0zaiZpPVlCYmZTS0ZyZ043eSZzPWNvbnN1bWVyc2VjcmV0Jng9ZmI-";
+			String category="top";
+			return String.format("%sappid=%s&pickupcategory=%s",apiURL,appid,category);
+		}
+		public static String httpGet(String strURL){
+		try{
+			URL url =new URL(strURL);
+			URLConnection connection=url.openConnection();
+			connection.setDoInput(true);
+			InputStream stream=connection.getInputStream();
+			BufferedReader input=new BufferedReader(new InputStreamReader(stream));
+			String data="";
+			String tmp="";
+			while((tmp=input.readLine()) != null){
+			data+=tmp;
+			}
+			stream.close();
+			input.close();
+			return data;
+		} catch (Exception e){
+			return e.toString();
+		}
+	}
+}
