01bce6bc2425aaff8bd88abdaa8f53be924755c7
nssv-03 <nssv-03@MI-PC-047>
2014-05-28 13:22:27 +0900

    ニュースべた打ちで出力
    べた打ち出力できた

--- KOWebCom/src/com/kobayasi/kowebcom/Webcom1.java
+++ KOWebCom/src/com/kobayasi/kowebcom/Webcom1.java
@@ -1,22 +1,31 @@
 package com.kobayasi.kowebcom;
 
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Xml;
 import android.widget.TextView;
 
 public class Webcom1 extends Activity {
 	private TextView mView;
 
+	static private String mArticleTitle[];
+	static private String mArticleURL[];
+	static private int mArticleNum;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_webcom1);
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
 		mView=(TextView)findViewById(R.id.view);
 		mView.setText(new String(httpGet(createURL())));
 		}
@@ -32,17 +41,42 @@ public class Webcom1 extends Activity {
 			URLConnection connection=url.openConnection();
 			connection.setDoInput(true);
 			InputStream stream=connection.getInputStream();
-			BufferedReader input=new BufferedReader(new InputStreamReader(stream));
+			readXML(stream);
 			String data="";
-			String tmp="";
-			while((tmp=input.readLine()) != null){
-			data+=tmp;
+			for(int i=0;i<mArticleNum;i++){
+				data+=mArticleTitle[i];
 			}
 			stream.close();
-			input.close();
 			return data;
 		} catch (Exception e){
 			return e.toString();
+		}
+	}
+	public static void readXML(InputStream stream)
+		throws XmlPullParserException{
+		try{
+			XmlPullParser myxmlPullParser=Xml.newPullParser();
+			myxmlPullParser.setInput(stream,"UTF-8");
+
+			int cntTitle =0;
+			int cntAddress =0;
+			for(int e = myxmlPullParser.getEventType(); e != XmlPullParser.END_DOCUMENT;e = myxmlPullParser.next()){
+				if(e ==XmlPullParser.START_TAG){
+					if(myxmlPullParser.getName().equals("ResultSet")){
+						mArticleNum =Integer.parseInt(myxmlPullParser.getAttributeValue(null,"totalResultsReturned"));
+						mArticleTitle=new String[mArticleNum];
+						mArticleURL=new String[mArticleNum];
+					} else if(myxmlPullParser.getName().equals("Title")){
+					mArticleTitle[cntTitle]=myxmlPullParser.nextText();
+					cntTitle++;
+					}else if(myxmlPullParser.getName().equals("SmartphoneUrl")){
+						mArticleURL[cntAddress]=myxmlPullParser.nextText();
+						cntAddress++;
+					}
+			}
+		}
+	}catch (XmlPullParserException e){
+		}catch(IOException e){
 		}
 	}
 }
